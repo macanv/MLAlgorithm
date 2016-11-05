89ba58b994a2f6e264f6ed1e837503650ec78c7d
389631699@qq.com <389631699@qq.com>
2016-11-06 01:16:39 +0800

    implement

--- src/canma/dmml/logisticRegression/LogisticRegression.java
+++ src/canma/dmml/logisticRegression/LogisticRegression.java
@@ -36,59 +36,105 @@ public class LogisticRegression {
     /**
      * 测试数据集
      */
-    private ArrayList<int[]> trainData;
+    private Map<Integer, List<Integer>> trainData;
     /**
      * 训练数据集
      */
-    private ArrayList<int[]> testData;
+    private Map<Integer, List<Integer>> testData;
 
     private double[] theta;
     /**
      * 初始化Model 设置训练集和测试数据集
-     * @param trainData  训练数据集
-     * @param testData  测试数据集
      */
-
-    public  LogisticRegression(){
-        theta = new double[10];
+    public LogisticRegression(){
+        //theta = new double[trainData.size()];
     }
-    public LogisticRegression(ArrayList<int[]> trainData, ArrayList<int[]> testData, int iter){
-        this.trainData = trainData;
-        this.testData = testData;
-        Iter = iter;
 
+
+    /**
+     * 根据训练集，求解LR的参数
+     * @param trainData  训练数据集
+     * @param iter  训练的迭代次数
+     */
+    public void trainLR(Map<Integer, List<Integer>> trainData, int iter){
         theta = new double[trainData.size()];
-    }
+        //迭代iter次训练数据
+        for (int i = 0; i < iter; ++i){
+            for (Map.Entry<Integer, List<Integer>> entry : trainData.entrySet()){ //对于每一个样本
+                int label = entry.getKey();
+                List<Integer> data = entry.getValue();
+                //计算当前样本的预测值
+                double prediction = clacClassification(data);
 
-
-    public void trainLR(){
-
-    }
-
-
-    public Map<Integer, List<Integer>> loadData(String path){
-        Map<Integer, List<Integer>> data  = new HashMap<>();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
-            String line;
-            while ((line = reader.readLine()) != null){
-                StringTokenizer tokenizer = new StringTokenizer(line);
-                ArrayList<Integer> linedata = new ArrayList<>();
-                while (tokenizer.hasMoreTokens()){
-                    Integer word = Integer.parseInt(tokenizer.nextToken());
-                    linedata.add(word);
+                double[] preTheta = new double[theta.length];
+                for (int j = 0; j < theta.length; ++j){
+                    preTheta[j] = theta[j];
+                    theta[j] = theta[j] + alpha * (label - prediction) * data.get(j);
                 }
-                data.put(linedata.get(0), linedata.subList(1,linedata.size() -1));
+
+                //如果已经迭代收敛了，停止训练
+                int j = 0;
+                for (; j < theta.length; ++j) {
+                    if (preTheta[j] - theta[j] > MIN){
+                        break;
+                    }
+                }
+                if (j >= theta.length){
+                    break;
+                }
+            }
+        }
+    }
+
+    /**
+     * 计算每一个样本的预测值
+     * @param x
+     * @return
+     */
+    public double  clacClassification(List<Integer> x){
+        double logit = .0;
+        for (int i=0; i< theta.length;i++)  {
+            logit += theta[i] * x.get(i);
+        }
+        return sigmoid(logit);
+    }
+
+    /**
+     * 计算最后的sigmoid 函数值
+     * @param z 计算出来的线性值
+     * @return 返回预测值
+     */
+    public double sigmoid(double z){
+        return 1.0 / (1.0 + Math.exp(-z));
+    }
+
+
+
+
+    /**
+     * 利用训练好的逻辑回归参数，来进行数据的二分类
+     * @param testData  需要分类的数据
+     * @return 返回分类的正确率
+     */
+    public double LRClassification(Map<Integer, List<Integer>> testData){
+        int p_right = 0;
+        double[] reslut = new  double[testData.size()];
+
+        int i = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : testData.entrySet()){
+            double res = clacClassification(entry.getValue());
+            //为了查看每次预测值，可以不保存
+            reslut[i++] = res;
+            res -= entry.getKey();
+//            如果结果在误差范围内，认为预测正确 并记下正确的次数
+            if (Math.abs(res) < alpha) {
+                p_right ++;
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
         }
 
-        return data;
-
+        return (p_right * 1.0) / testData.size();
     }
-
 
 
 
--- src/canma/dmml/logisticRegression/TestLR.java
+++ src/canma/dmml/logisticRegression/TestLR.java
@@ -2,6 +2,9 @@ package canma.dmml.logisticRegression;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.*;
 
 /**
@@ -10,29 +13,75 @@ import java.util.*;
 public class TestLR {
     @Test
     public void TestLR(){
-        String  path = "data/LR/train.txt";
-        LogisticRegression logisticRegression = new LogisticRegression();
-        Map<Integer, List<Integer>> data = logisticRegression.loadData(path);
+        String path = "data/LR/train.txt";
+
+        Map<Integer, List<Integer>> data = loadData(path);
         Map<Integer, List<Integer>> trainData = new HashMap<>();
         Map<Integer, List<Integer>> testData = new HashMap<>();
         partitionData(data,trainData, testData);
-        System.out.println(data);
+
+        LogisticRegression logisticRegression = new LogisticRegression();
+        logisticRegression.trainLR(trainData, 100);
+        double p_rigth = logisticRegression.LRClassification(testData);
+        System.out.printf("p_right = " + p_rigth);
     }
 
+    /**
+     * 加载数据
+     * @param path  数据所在的目录
+     * @return  返回数据
+     */
+    public static Map<Integer, List<Integer>> loadData(String path){
+        Map<Integer, List<Integer>> data  = new HashMap<>();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+            String line;
+            while ((line = reader.readLine()) != null){
+                StringTokenizer tokenizer = new StringTokenizer(line);
+                ArrayList<Integer> linedata = new ArrayList<>();
+                while (tokenizer.hasMoreTokens()){
+                    Integer word = Integer.parseInt(tokenizer.nextToken());
+                    linedata.add(word);
+                }
+                //这个可以将sublist的返回值转化为ArrayList
+//                ArrayList<Integer> d = new ArrayList<Integer>(linedata.subList(1, linedata.size()));
+                data.put(linedata.get(0), linedata.subList(1,linedata.size()));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return data;
+
+    }
+
+
+    /**
+     * 将数据集分位80%的训练集，20%的测试集
+     * @param data 总体数据
+     * @param trainData   训练数据
+     * @param testData  测试数据
+     */
     public static void partitionData(Map<Integer, List<Integer>> data, Map<Integer, List<Integer>> trainData, Map<Integer, List<Integer>> testData){
-        trainData = data;
+        trainData = new HashMap<>(data);
         int numberOfTest = (int)(data.size() * 0.2);
-        Random random = new Random(data.size());
-        Set<Integer> index = data.keySet();
+        Random random = new Random();
+        int[] index = new int[numberOfTest];
+
+        for (int i = 0; i < numberOfTest ; ++i){
+            index[i] = random.nextInt(data.size());
+        }
 
         int temp = 0;
-        for (Integer i : index){
-            if (temp++ > numberOfTest){
-                break;
+        int i = 0;
+        for (Map.Entry<Integer, List<Integer>> entry : data.entrySet()){
+            if (temp++ == index[i]) {
+                testData.put(entry.getKey(), entry.getValue());
+                trainData.remove(entry.getKey());
+                i++;
             }
-            int r = random.nextInt();
-            testData.put(i, trainData.get(i));
-            trainData.remove(i);
+
         }
 
     }
