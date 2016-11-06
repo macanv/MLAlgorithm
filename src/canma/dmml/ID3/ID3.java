1c3dea4e65671007d676ea51c45b16e78b1d8001
macan <389631699@qq.com>
2016-11-06 23:17:18 +0800

    LDA

---
+++ src/canma/dmml/ID3/ID3.java
@@ -0,0 +1,12 @@
+package canma.dmml.ID3;
+
+/**
+ * Created by macan on 2016/11/6.
+ *
+ * ID3 决策树算法实现
+ */
+
+
+public class ID3 {
+
+}
---
+++ src/canma/dmml/LinearDA/LinearDA.java
@@ -0,0 +1,10 @@
+package canma.dmml.LinearDA;
+
+/**
+ * Created by macan on 2016/11/6.
+ *
+ * linear discrimination analysis  线性语义分析
+ */
+public class LinearDA {
+
+}
